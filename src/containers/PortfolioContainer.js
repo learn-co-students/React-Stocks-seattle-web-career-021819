0cd70eef99080212aec02cc9e40b0fc840147980
Logan Wohlers <lawohlers@gmail.com>
2019-04-27 20:56:29 -0700

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -4,11 +4,16 @@ import Stock from '../components/Stock'
 class PortfolioContainer extends Component {
 
   render() {
+    // console.log(this.props.portfolioStocks)
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            (this.props.portfolioStocks === undefined || this.props.portfolioStocks.length === 0) ? 
+            null :
+            this.props.portfolioStocks.map(ss=>{
+              return <Stock key={Math.random()*1000} stock={ss} onStockMethod={this.props.onStockSell} />
+            })
           }
       </div>
     );
