733bded8100ac205d2b91f9ec02fbe1066747577
Joshua Loran <joshua.loran@gmail.com>
2019-04-29 11:10:22 -0700

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -7,9 +7,9 @@ class StockContainer extends Component {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.props.stocks.map((stock, idx) => {
+          return <Stock onClickMethod={this.props.addStockToPortfolio} stock={stock} key ={idx}/>
+        })}
       </div>
     );
   }
