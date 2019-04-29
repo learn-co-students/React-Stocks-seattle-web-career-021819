3e0ccbc6c6a9f852b260f9e6e232843ad6d687d6
github username <github email address>
2019-04-28 17:49:56 -0700

    complete

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
+  constructor(props){
+    super(props)
+  }
 
   render() {
     return (
-      <div>
-        <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
-      </div>
+          <Fragment>
+          <div>
+            <h2>Stocks</h2>
+            {this.props.stocks.map((stock, index) => {
+              return <Stock key={index} stock={stock} purchaseStock={this.props.purchaseStock}/>
+            })}
+          </div>
+          </Fragment>
     );
   }
 
