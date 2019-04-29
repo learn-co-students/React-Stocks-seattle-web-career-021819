a3c00e159a65776d7943a73dea655c28bbb4f48a
github username <github email address>
2019-04-29 10:57:33 -0700

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,58 @@
-import React from 'react';
+import React from "react";
 
-const SearchBar = () => {
-  return (
-    <div>
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
 
-      <strong>Sort by:</strong>
-      <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
-      </label>
-      <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
-        Price
-      </label>
-      <br/>
+  handleAlphaSort = ev => {
+    this.props.alphaSort(ev.target.value);
+    this.setState({ selected: "Alphabetically" });
+  };
 
-      <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
-        </select>
-      </label>
+  handlePriceSort = ev => {
+    this.props.priceSort(ev.target.value);
+    this.setState({ selected: "Price" });
+  };
 
+  render() {
+    return (
+      <div>
+        <strong>Sort by:</strong>
+        <label>
+          <input
+            type="radio"
+            value="Alphabetically"
+            checked={this.state.selected === "Alphabetically" ? true : false}
+            onChange={ev => this.handleAlphaSort(ev)}
+          />
+          Alphabetically
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Price"
+            checked={this.state.selected === "Price" ? true : false}
+            onChange={ev => this.handlePriceSort(ev)}
+          />
+          Price
+        </label>
+        <br />
 
-    </div>
-  );
+        <label>
+          <strong>Filter:</strong>
+          <select onChange={ev => this.props.filterStocks(ev.target.value)}>
+            <option value="Tech">Tech</option>
+            <option value="Sportswear">Sportswear</option>
+            <option value="Finance">Finance</option>
+          </select>
+        </label>
+      </div>
+    );
+  }
 }
-
 
 export default SearchBar;
