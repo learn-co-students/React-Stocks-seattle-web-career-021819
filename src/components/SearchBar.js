7e3ad7a06a3137f82e1075101b187c128ae21f82
github username <github email address>
2019-04-29 18:01:45 -0700

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return (
-    <div>
+class SearchBar extends Component {
+  constructor() {
+    super()
+    this.state = {
+      sortBy: 'Alphabetically',
+    }
+    this.handleCheckChange = this.handleCheckChange.bind(this)
+    this.handleFilter = this.handleFilter.bind(this)
+  }
 
-      <strong>Sort by:</strong>
-      <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
-      </label>
-      <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
-        Price
-      </label>
-      <br/>
+  handleCheckChange(e) {
+    this.setState({sortBy: e.target.value})
+    this.props.sortBy(e.target.value)
+  }
 
-      <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
-        </select>
-      </label>
+  handleFilter(e) {
+    let filter = e.target.value;
+    this.props.filter(filter)
+  }
+
+  render() {
+    return (
+      <div>
+
+        <strong>Sort by:</strong>
+        <label>
+          <input type="radio"
+          value="Alphabetically"
+          checked={this.state.sortBy === 'Alphabetically'}
+          onChange={this.handleCheckChange}/>
+          Alphabetically
+        </label>
+        <label>
+          <input type="radio"
+          value="Price"
+          checked={this.state.sortBy === 'Price'}
+          onChange={this.handleCheckChange}/>
+          Price
+        </label>
+        <br/>
+
+        <label>
+          <strong>Filter:</strong>
+          <select onChange={ e => {this.handleFilter(e)}}>
+            <option value="Tech">Tech</option>
+            <option value="Sportswear">Sportswear</option>
+            <option value="Finance">Finance</option>
+          </select>
+        </label>
 
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,16 +1,20 @@
 import React from 'react'
-
-const Stock = () => (
-  <div>
+// id: 1,
+// ticker: "GOOG",
+// name: "Google",
+// type: "Tech",
+// price: 1194.8
+const Stock = (props) => (
+  <div onClick={() => props.handleClick(props.stock)}>
 
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+        <h5 className="card-title">
+          {props.stock.name}
+        </h5>
+        <p className="card-text">
+          {props.stock.ticker + ': ' + props.stock.price}
+        </p>
       </div>
     </div>
 
